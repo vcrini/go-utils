1ee715c7041f9e81ae9cd202b7ed8392e7799f49
valerio <vcrini@gmail.com>
2023-07-25 11:41:24 +0200

    using space instead '+'

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func Exe(s []string) string {
 	cmd := exec.Command(s[0], s[1:]...)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot run:\n%s\n%s\n '%s'", strings.Join(s, "+"), out, err)
+		fmt.Fprintf(os.Stderr, "Cannot run:\n%s\n%s\n '%s'", strings.Join(s, " "), out, err)
 		os.Exit(1)
 	}
 	return string(out)
